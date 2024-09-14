d19e71203326f33cd27f125e8a135ec3fa7a592c
TahroO <bernd.reusch@hotmail.com>
2024-09-15 01:39:06 +0200

    cleanUp

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -11,25 +11,9 @@ import {NgIf} from "@angular/common";
   styleUrl: './square.component.css'
 })
 export class SquareComponent {
-  // constructor() {
-  //   // setInterval(() => this.rand = Math.random(), 500)
-  // }
-  // rand = Math.random();
   @Input() value: 'X' | 'O';
-  color: string;
 
   constructor() {
     this.value = 'X';
-    this.color = this.getColor();
-  }
-
-  protected getColor(){
-    if (this.value === 'X') {
-      return 'blue';
-    }
-    if (this.value === 'O') {
-      return 'red';
-    }
-    return 'grey';
   }
 }
